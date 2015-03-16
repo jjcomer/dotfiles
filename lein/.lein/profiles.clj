bd1c27c8c94d7a47f4fcbd645d2a7fb9dcab74c0
Josh Comer <jcomer@liveops.com>
2015-03-16 13:06:27 -0300

    Add start command

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -6,13 +6,14 @@
                   [codox "0.8.10"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-exec "0.3.4"]
-                  [test2junit "1.1.0"]
-                  [venantius/ultra "0.3.2"]]
+                  [test2junit "1.1.0"]]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[org.clojure/tools.nrepl "0.2.7"]]
         :aliases {"ccheck" ^{:doc "Clean and check the source"}
                   ["do" ["clean"] ["check"]]
                   "cinstall" ^{:doc "Clean and install locally"}
                   ["do" ["clean"] ["install"]]
+                  "start" ^{:doc "Trampoline run"}
+                  ["trampoline" "run"]
                   "sancient" ^{:doc "Check all deps including snapshots"}
                   ["ancient" ":allow-all"]}}}
