3c461b5d295d732eaa57d816abab9341229f8c37
Josh Comer <jcomer@liveops.com>
2015-04-15 10:01:53 -0300

    Update lein deps

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,19 +1,21 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
-                  [lein-ancient "0.6.2"]
+                  [lein-ancient "0.6.6"]
                   [jonase/eastwood "0.2.1"]
                   [lein-environ "1.0.0"]
-                  [refactor-nrepl "0.3.0-SNAPSHOT"]
-                  [codox "0.8.10"]
+                  [refactor-nrepl "1.0.2-SNAPSHOT"]
+                  [codox "0.8.11"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-exec "0.3.4"]
+                  [quickie "0.3.6"]
                   [test2junit "1.1.0"]]
-        :ultra {:color-scheme :solarized_dark}
-        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
         :aliases {"ccheck" ^{:doc "Clean and check the source"}
                   ["do" ["clean"] ["check"]]
                   "cinstall" ^{:doc "Clean and install locally"}
                   ["do" ["clean"] ["install"]]
                   "start" ^{:doc "Trampoline run"}
                   ["trampoline" "run"]
+                  "cstart" ^{:doc "Clean and start"}
+                  ["do" ["clean"] ["start"]]
                   "sancient" ^{:doc "Check all deps including snapshots"}
                   ["ancient" ":allow-all"]}}}
