517a597304b99f401d17b56be3e6dd9246f17c84
Josh Comer <jcomer@liveops.com>
2015-04-24 05:29:50 -0300

    Update lein deps

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -2,11 +2,11 @@
                   [lein-ancient "0.6.6"]
                   [jonase/eastwood "0.2.1"]
                   [lein-environ "1.0.0"]
-                  [refactor-nrepl "1.0.2-SNAPSHOT"]
+                  [refactor-nrepl "1.0.4-SNAPSHOT"]
                   [codox "0.8.11"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-exec "0.3.4"]
-                  [quickie "0.3.6"]
+                  [quickie "0.3.9"]
                   [test2junit "1.1.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
         :aliases {"ccheck" ^{:doc "Clean and check the source"}
