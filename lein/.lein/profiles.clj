71e300c1060e81c34365c3d40f0a1bc5db798821
Josh Comer <jcomer@liveops.com>
2016-01-27 15:17:12 -0400

    Upgrade cider

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -19,5 +19,5 @@
                   "sancient" ^{:doc "Check all deps including snapshots"}
                   ["ancient" ":allow-all"]}}
  :repl {:plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.0"]]
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
