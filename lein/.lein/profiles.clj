ed2276d1e6a53f23090ed0affc246eee7790b4d7
Josh Comer <jcomer@liveops.com>
2016-03-04 06:46:14 -0400

    Add lein-pprint

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -5,7 +5,8 @@
                   [codox "0.9.4"]
                   [lein-exec "0.3.6"]
                   [quickie "0.4.1"]
-                  [test2junit "1.2.1"]]
+                  [test2junit "1.2.1"]
+                  [lein-pprint "1.1.1"]]
         :aliases {"ccheck" ^{:doc "Clean and check the source"}
                   ["do" ["clean"] ["check"]]
                   "cinstall" ^{:doc "Clean and install locally"}
